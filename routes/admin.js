919b9058bcd03cc0958006faadc820dd335c3f5d
riya <riyabharti.3012@gmail.com>
2020-06-07 22:18:39 +0530

    Apis added in admin API

--- routes/admin.js
+++ routes/admin.js
@@ -18,6 +18,7 @@ router.post('/addEvent',Auth.authenticateAdmin, function(req,res){
                 error: err
             });
         }
+        //Never gets satisfied as we create category before adding events
         if(data == null)
         {
             var newCategoryEvent = new Category(req.body);
@@ -38,10 +39,19 @@ router.post('/addEvent',Auth.authenticateAdmin, function(req,res){
         }
         else
         {
-            // data.events=[...data.events,req.body.events];
-            res.status(200).json({
-                'status': false,
-                'message': "Category already exists"
+            data.events=[...data.events,...req.body.events];
+            data.save().then(item=> {
+                res.status(200).json({
+                    'status':true,
+                    'message':"Events added in the category",
+                    'data':item
+                })
+            }).catch(err=>{
+                res.status(500).json({
+                'status':false,
+                'message':"Event addition error",
+                'error':err
+                })
             })
         }
     })   
@@ -117,6 +127,118 @@ router.get('/fetchUsers',Auth.authenticateAdmin, function(req,res){
     })
 })
 
+//Add Category
+router.post('/addCategory',Auth.authenticateAdmin,function(req,res){
+    var categoryData = new Category(req.body);
+    new Category(categoryData).save().then(
+        item=> {
+            if(item)
+            {
+                return res.status(200).json({
+                    status: true,
+                    message: "Category Addition successful",
+                    data: item
+                })
+            }
+            else
+            {
+                return res.status(500).json({
+                    status: false,
+                    message: "Category Addition Failed! Try again..",
+                    error: "Unknown"
+                  });
+            }
+        }
+    )
+    .catch(err => {
+        return res.status(500).json({
+          status: false,
+          message: "Category Addition Failed! Server Error..",
+          error: err
+        });
+      });
+})
+
+//Delete Category
+router.post('/deleteCategory',Auth.authenticateAdmin,function(req,res){
+    Category.findOneAndRemove({category: req.body.category},(err, deletedCategory) => {
+        if (err)
+        {
+            return res.status(500).json({
+                status: false,
+                message: "Deleting Category Failed! Server Error..",
+                error: err
+            });
+        }
+        if(deletedCategory)
+        {
+            return res.status(200).json({
+                status: true,
+                message: "Deleted Category successfully",
+                user: deletedCategory
+              });
+        }
+        else
+        {
+            return res.status(500).json({
+                status: false,
+                message: "Delete Category Failed",
+                error: 'Unknown'
+            });
+        }
+    })
+})
+
+//Delete Event
+router.post('/deleteEvent',Auth.authenticateAdmin,function(req,res){
+    Category.findOne({category: req.body.category}, (err,item)=> {
+        if(err)
+        {
+            return res.status(500).json({
+                status: false,
+                message: "Deleting Event Failed! Server Error..",
+                error: err
+            });
+        }
+        if(item)
+        {
+            if(item.events[req.body.index].name == req.body.eventName)
+            {
+                item.events.splice(req.body.index,1);
+                item.save().then(item => {
+                    return res.status(200).json({
+                        status: true,
+                        message: "Delete Event successful",
+                        data: item
+                    });
+                })
+                .catch(err => {
+                    return res.status(500).json({
+                        status: false,
+                        message: "Delete Event Failed",
+                        error: err
+                    });
+                })
+            }
+            else
+            {
+                return res.status(500).json({
+                    status: false,
+                    message: "Delete Event Failed",
+                    error: 'Wrong event details'
+                });
+            }
+        }
+        else
+        {
+            return res.status(500).json({
+                status: false,
+                message: "Category does not exist!",
+                error: 'Category find error'
+            });
+        }
+    })
+})
 
 module.exports=router;
 
